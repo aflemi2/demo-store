aca9e73537b8b00a7466013b86045b974ba79dee
Alan Fleming <32113114+aflemi2@users.noreply.github.com>
2023-09-14 16:42:46 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,11 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>NACS Demo Store</Title>
+             <div style="position: absolute;border:none;margin: auto; left:0; right:0; width:450px">
+          <img src="https://i.stack.imgur.com/AR3kw.png" style="z-index:1 ;position:absolute"   />
+          <iframe src="https://demo.nopcommerce.com/" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" style="z-index: 2;position: relative;width: 310px;left:53px;height: 640px;top: 60px;border-bottom-right-radius: 30px;border-bottom-left-radius: 30px;border:0"></iframe>
+      </div>
     </main>
   );
 }
