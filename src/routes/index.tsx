8c4fc39c69502c327a066d4b513d7bea03bfd597
Alan Fleming <32113114+aflemi2@users.noreply.github.com>
2023-09-18 10:18:15 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <Title>NACS Demo Store</Title>
              <div style="position: absolute;border:none;margin: auto; left:0; right:0; width:450px">
-          <img src="../../../phone.png" style="z-index:1 ;position:absolute"   />
+          <img src="phone.png" style="z-index:1 ;position:absolute"   />
           <iframe src="https://demo.nopcommerce.com/" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" style="z-index: 2;position: relative;width: 310px;left:53px;height: 640px;top: 60px;border-bottom-right-radius: 30px;border-bottom-left-radius: 30px;border:0"></iframe>
       </div>
     </main>
